d611753e0effeddc57b677d6dfd9d5f5a39a61f9
sim <sim@arch-vile.fritz.box>
2020-11-14 12:43:07 +0100

    trace bench

---
+++ examples/trace.rs
@@ -0,0 +1,32 @@
+use std::time::Instant;
+
+use crystal_planes::crystal::{self, math::prelude::*};
+
+fn bench(bm: &dyn crystal::Bitmap) {
+    let t0 = Instant::now();
+    let start = Point3i::new(40, 180, 40);
+    let mut count = 0;
+    let mut count_all = 0;
+    for ((x, y, z), v) in bm.cell_iter() {
+        count_all += 1;
+        if *v {
+            continue;
+        }
+        if bm.occluded(
+            start,
+            Point3i::new(x as i32, y as i32, z as i32),
+            None,
+            None,
+        ) {
+            count += 1;
+        }
+    }
+    println!("count: {} {} {:?}", count, count_all, t0.elapsed());
+}
+
+fn main() {
+    let bm = crystal::read_map("assets/maps/hidden_ramp.txt").expect("could not read file");
+    bench(&*bm);
+    // let bm = Box::new(crystal::accel::OctreeBitmap::wrap(bm));
+    //bench(&*bm);
+}
